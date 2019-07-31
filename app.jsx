8d7f6a41bf843ef57964733f03cc3aebf8d52b28
Ramprits <rampritsahani@outlook.in>
2019-07-31 09:47:52 +0530

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <React.Fragment>
+        Welcome
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
